d3e9a8139a6dedac7e343bb5af972ea97c817f53
webshoposcar1164 <cheaptrip1164@gmail.com>
2023-12-23 16:56:32 +0900

    ノートの追加・削除機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import uuid from "react-uuid";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [notes, setNotes] = useState([]);
+	const [activeNote, setActiveNote] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const onAddNote = () => {
+		console.log("新しくノートが追加されました。");
+		const newNote = {
+			id: uuid(),
+			title: "新しいノート",
+			content: "新しいノートの内容",
+			modDate: Date.now(),
+		};
+		setNotes([...notes, newNote]);
+		console.log(notes);
+	};
+
+	const onDeleteNote = (id) => {
+		console.log("ノートが削除されました。id:" + id);
+		const filterNotes = notes.filter((note) => note.id !== id);
+		setNotes(filterNotes);
+	};
+
+	return (
+		<>
+			<div className="App">
+				<Sidebar
+					notes={notes}
+					onAddNote={onAddNote}
+					onDeleteNote={onDeleteNote}
+					activeNote={activeNote}
+					setActiveNote={setActiveNote}
+				/>
+				<Main />
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = ({
+	onAddNote,
+	onDeleteNote,
+	notes,
+	activeNote,
+	setActiveNote,
+}) => {
+	return (
+		<div className="app-sidebar">
+			<div className="app-sidebar-header">
+				<h1>ノート</h1>
+				<button onClick={onAddNote}>追加</button>
+			</div>
+			<div className="app-sidebar-notes">
+				{notes.map((note) => {
+					return (
+						<div
+							className={`app-sidebar-note ${
+								note.id === activeNote && "active"
+							}`}
+							key={note.id}
+							onClick={() => setActiveNote(note.id)}
+						>
+							<div className="sidebar-note-title">
+								<strong>{note.title}</strong>
+								<button onClick={() => onDeleteNote(note.id)}>削除</button>
+							</div>
+							<p>{note.content}</p>
+							<small>
+								{new Date(note.modDate).toLocaleDateString("ja-JP", {
+									hour: "2-digit",
+									minute: "2-digit",
+								})}
+							</small>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
